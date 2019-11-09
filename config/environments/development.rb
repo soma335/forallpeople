f29b8dabd3da5e579fbc7b0ba0701cc07a9fe5f9
Your Name <your.email@example.com>
2019-11-09 16:01:25 +0000

    Update Gemfile for Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,10 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'example.com' # ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
