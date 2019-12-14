a0788bc83be368f3ba5c7a5c5addb6f6d4f1e0b8
Your Name <your.email@example.com>
2019-12-14 03:06:59 +0000

    Add user microposts

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-　　　Rails.application.config.assets.precompile += %w( *.eot *.woff *.ttf *.svg *.otf *.png *.jpg *.jpeg *.gif vendor.css vendor.js )
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
