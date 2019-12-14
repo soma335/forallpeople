954157921e383c589439181e1515cadcd89542ac
Your Name <your.email@example.com>
2019-12-14 01:18:58 +0000

    Add password reset

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -7,8 +7,8 @@ if Rails.env.production?
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: '0welcometomachine1@gmail.com',
-    password: 'jxmcxaxyqzlhvqlf',
+    user_name:  ENV['GMAIL'],
+    password:  ENV['GMAILPASS'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
