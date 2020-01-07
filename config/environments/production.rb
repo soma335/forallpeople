d4f57d20533ae8c8bb80e0d4a3be589856bf536b
Your Name <your.email@example.com>
2020-01-07 11:37:39 +0000

    sin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV[SENDGRID_PASSWORD],
+    :password       => ENV[SENDGRID_USERNAME],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
