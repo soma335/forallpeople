c63aac730ca66c295fc123ebcf83d8daf22e7ce8
Your Name <your.email@example.com>
2020-01-07 11:17:47 +0000

    sin

--- db/migrate/20200107051852_remove_album_from_micropost.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAlbumFromMicropost < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :microposts, :name, :string
-  end
-end
--- db/migrate/20200107110824_add_name_to_micropost.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToMicropost < ActiveRecord::Migration[5.1]
-  def change
-    add_column :microposts, :name, :string
-  end
-end
